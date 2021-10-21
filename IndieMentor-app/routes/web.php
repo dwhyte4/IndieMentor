724606def1e66412ad92140ee15f0974468d19b9
dwhyte4 <dwhyte4@hotmail.com>
2021-10-21 14:47:31 +0100

    Installation of Laravel Breeze, completion of sidebar and navbar templates, completion of initial migrations "User, Document" tables.

--- IndieMentor-app/routes/web.php
+++ IndieMentor-app/routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
 Route::get('/master', function () {
-    return view('Layout.master');
+    return view('template.master');
 });
